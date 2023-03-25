d4bcd0403ae0da3e96fb2805ed8d75bcc65015da
PurushothamaG96 <purushothamachar96@gmail.com>
2023-03-25 16:34:05 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Signup from './components/Signup';
 import Signin from './components/Signin';
+import Home from './components/Home';
+import Addposts from './components/Addposts';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Signin/>}/>
         <Route path='/signup' element={<Signup/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/add' element={<Addposts/>}/>
       </Routes>
     </BrowserRouter>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,57 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import Nav from './Nav';
+import "./home.css"
+import { useNavigate } from 'react-router-dom';
 function Home(props) {
-    return (
-        <div>
+    const [arr, setArr] = useState([])
+    const navigate = useNavigate()
+    const token=JSON.parse(localStorage.getItem("notToken"))
+    let month = ["Jan", "Feb", "March", "April", "May", "June", "July", "August", "Sept", "Oct", "Nov", "Dec"]
+    useEffect(()=>{
+        handleData()
+    }, [])
+
+    function handleData(){
+        fetch("https://clean-headscarf-calf.cyclic.app/v1/post", {
+            method:"get",
+            headers: {
+                "Authorization":token
+                }
             
-        </div>
+        }).then((res)=>res.json())
+        .then((data)=>{
+            setArr(data)
+        }).catch((e)=>{
+            console.log(e)
+        })
+    }
+    return (
+        <>
+        <Nav handleData={handleData}/>
+            <div className='home-container'>
+                {
+                    arr.map((val, i)=>{
+                        let times = val.createdAt.split("T")
+                        let m = parseInt(times[0].split("-")[1])
+                        console.log(m)
+                        return(
+                            <div className='cards'>
+                                <div className='time'>
+                                    <p >
+                                    <span style={{marginRight:"10px",fontWeight:700}}>{month[m-1]}</span>
+                                    <span style={{marginRight:"10px",fontWeight:700}}>{times[0]}</span>
+                                    <span>{times[1]}</span>
+                                    </p>
+                                </div>
+                                <h1>{val.title}</h1>
+                                <p>{val.description}</p>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </>
+        
     );
 }
 
